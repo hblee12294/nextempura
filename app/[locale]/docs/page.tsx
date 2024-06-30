f44d72b5c56e9320dafb9112ff330afff6db7452
Hongbin Li <hblee12294@gmail.com>
2024-06-29 17:57:41 -0700

    feat: add sample products

--- app/[locale]/docs/page.tsx
+++ app/[locale]/docs/page.tsx
@@ -1,9 +1,25 @@
 import { useTranslations } from "next-intl";
 
 import styles from "./page.module.scss";
+import { PRODUCTS } from "@/configs/products";
+import { Link } from "@/navigation";
 
 export default function Docs() {
-  const t = useTranslations("Header");
+  const t = useTranslations("Products");
 
-  return <div className={styles.page}>Default Docs</div>;
+  return (
+    <div className={styles.page}>
+      <ul className={styles.productsGrid}>
+        {PRODUCTS.map(({ id, docPath }) => (
+          <li key={id} className={styles.productBlock}>
+            <Link href={`/docs/${docPath}`}>
+              <div className={styles.coverImage}></div>
+              <h3>{t(`${id}.name`)}</h3>
+              <p>{t(`${id}.description`)}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
---
+++ configs/products.ts
@@ -0,0 +1,19 @@
+interface Product {
+  id: string;
+  docPath: string;
+}
+
+export const PRODUCTS: Product[] = [
+  {
+    id: "ebi",
+    docPath: "/ebi",
+  },
+  {
+    id: "sakana",
+    docPath: "/sakana",
+  },
+  {
+    id: "kabocha",
+    docPath: "/kabocha",
+  },
+];
