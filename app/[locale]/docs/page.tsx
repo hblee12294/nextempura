63f8cdd76aefebcc615e988bc1bb57b43268097e
Hongbin Li <hblee12294@gmail.com>
2024-06-29 21:02:58 -0700

    feat: support importing svg as component

--- app/[locale]/docs/page.tsx
+++ app/[locale]/docs/page.tsx
@@ -3,6 +3,7 @@ import { useTranslations } from "next-intl";
 import styles from "./page.module.scss";
 import { PRODUCTS } from "@/configs/products";
 import { Link } from "@/navigation";
+import BlobIcon from "@/svgs/blob.svg";
 
 export default function Docs() {
   const t = useTranslations("Products");
@@ -10,15 +11,19 @@ export default function Docs() {
   return (
     <div className={styles.page}>
       <ul className={styles.productsGrid}>
-        {PRODUCTS.map(({ id, docPath }) => (
-          <li key={id} className={styles.productBlock}>
-            <Link href={`/docs/${docPath}`}>
-              <div className={styles.coverImage}></div>
-              <h3>{t(`${id}.name`)}</h3>
-              <p>{t(`${id}.description`)}</p>
-            </Link>
-          </li>
-        ))}
+        {PRODUCTS.map(({ id, docPath }, i) => {
+          return (
+            <li key={id} className={styles.productBlock}>
+              <Link href={`/docs/${docPath}`}>
+                <div className={styles.coverImage}>
+                  <BlobIcon className={styles.icon}></BlobIcon>
+                </div>
+                <h3>{t(`${id}.name`)}</h3>
+                <p>{t(`${id}.description`)}</p>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
