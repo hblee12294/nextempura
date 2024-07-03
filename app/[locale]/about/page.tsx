1fd179b6f8d8c6e7800c96646fe3a20992998ff2
Hongbin Li <hblee12294@gmail.com>
2024-07-03 01:34:09 -0700

    feat: add basic about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -3,7 +3,27 @@ import { useTranslations } from "next-intl";
 import styles from "./page.module.scss";
 
 export default function About() {
-  const t = useTranslations("Header");
+  const t = useTranslations("About");
 
-  return <main className={styles.page}></main>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.blob1}></div>
+      <div className={styles.blob2}></div>
+      <div className={styles.blob3}></div>
+
+      <main className={styles.content}>
+        <div className={styles.titleSection}>
+          <h1 className={styles.title}>{t("title")}</h1>
+        </div>
+
+        <div className={styles.introSection}>
+          <p className={styles.intro}>{t("intro")}</p>
+        </div>
+
+        <div className={styles.useSection}>
+          <p className={styles.use}>{t("use")}</p>
+        </div>
+      </main>
+    </div>
+  );
 }
