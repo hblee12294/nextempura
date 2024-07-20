0dcf21f17b2dd855f1b83c38d8fd455df27aaca8
Hongbin Li <hblee12294@gmail.com>
2024-07-19 22:27:19 -0700

    build: upgrade next-intl to v3.17.2

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import createMiddleware from "next-intl/middleware";
 
 import { LOCALES, DEFAULT_LOCALE, LOCALE_PREFIX } from "@/configs/locales";
 
-const intlMiddleware = createMiddleware({
+export default createMiddleware({
   // A list of all locales that are supported
   locales: LOCALES,
 
@@ -13,16 +13,6 @@ const intlMiddleware = createMiddleware({
   // Don't use a prefix for the default locale
   localePrefix: LOCALE_PREFIX,
 });
-
-export default function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  // Detect if pathname contains encoded backslash
-  const maliciousURL = new RegExp(/(%5C|%5c)/i).test(pathname);
-  if (maliciousURL) return;
-
-  return intlMiddleware(request);
-}
 
 export const config = {
   // Match only internationalized pathnames
